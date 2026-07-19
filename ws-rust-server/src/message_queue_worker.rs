4a19afb979975595e4752eeed368c7554646a4b9
RAwearsPRADA <avtarkhanov.07@mail.ru>
2026-07-19 19:21:13 +0300

    Message queue worker optimized

--- ws-rust-server/src/message_queue_worker.rs
+++ ws-rust-server/src/message_queue_worker.rs
@@ -51,11 +51,23 @@ impl MessageQueueWorker {
     }
     
     pub async fn start(self) {
+        let mut redis_conn = match self.redis.get_multiplexed_tokio_connection().await {
+            Ok(connection) => {
+                println!("Successfully connected redis");
+                connection
+            }
+            Err(e) => {
+                println!("Error during connection redis: {}", e.to_string());
+                return;
+            }
+        };
         loop {
-            match self.start_message_queue_worker().await {
+            match self.start_message_queue_worker(&mut redis_conn).await {
                 Ok(inserted_count) => {
                     if inserted_count == 0 {
                         sleep(Duration::from_secs(1)).await;
+                    } else {
+                        sleep(Duration::from_millis(500)).await;
                     }
                 }
                 Err(error) => {
@@ -66,25 +78,20 @@ impl MessageQueueWorker {
         }
     }
 
-    pub async fn start_message_queue_worker(&self) -> Result<usize, String> {
-        let mut redis_conn = self.redis.get_multiplexed_tokio_connection()
+        pub async fn start_message_queue_worker(&self, redis_conn: &mut redis::aio::MultiplexedConnection) -> Result<usize, String> {
+        let redis_query: Option<Vec<String>> = redis_conn.lpop("queue:messages", std::num::NonZero::new(200))
             .await
             .map_err(|e| e.to_string())?;
-        let redis_query: Option<Vec<String>> = redis_conn.lpop("queue:messages", std::num::NonZero::new(1000))
-            .await
-            .map_err(|e| e.to_string())?;
+
         let messages_json = match redis_query {
-            Some(vector) => {
-                vector
-            },
-            _ => {
-                return Ok(0)
-            }
+            Some(vector) => vector,
+            _ => return Ok(0)
         };
+
         if messages_json.is_empty() {
             return Ok(0);
         }
-        
+
         let mut parsed_messages = Vec::new();
         for str in messages_json {
             if let Ok(parsed_msg) = serde_json::from_str::<Message>(&str) {
@@ -94,43 +101,67 @@ impl MessageQueueWorker {
         if parsed_messages.is_empty() {
             return Ok(0)
         }
-        
+
+        let inserted_len = parsed_messages.len();
+
         let mut sql = String::from(
-        "INSERT INTO \"Message\" (content, \"conversationId\", \"senderId\", \"createdAt\", \"isRead\", type) VALUES "
-    );
+            "WITH inserted_messages AS ( \
+                INSERT INTO \"Message\" (content, \"conversationId\", \"senderId\", \"createdAt\", \"isRead\", type) \
+                VALUES "
+        );
 
-    let mut placeholders = Vec::new();
-    let fields_count = 6; // Сколько полей вставляем
+        let mut insert_placeholders = Vec::new();
+        let fields_count = 6;
+        for i in 0..inserted_len {
+            let base = i * fields_count;
+            insert_placeholders.push(format!(
+                "(${}, ${}, ${}, ${}, ${}, ${}::\"MessageType\")",
+                base + 1, base + 2, base + 3, base + 4, base + 5, base + 6
+            ));
+        }
+        sql.push_str(&insert_placeholders.join(", "));
+        
+        sql.push_str(
+            " RETURNING content, \"conversationId\", \"senderId\", \"createdAt\", \"isRead\", type \
+             ), \
+             latest_messages AS ( \
+                SELECT DISTINCT ON (\"conversationId\") * FROM inserted_messages \
+                ORDER BY \"conversationId\", \"createdAt\" DESC \
+             ) \
+             UPDATE \"ConversationParticipant\" AS cp \
+             SET \
+                \"lastMessageContent\" = latest_messages.content, \
+                \"lastMessageTime\" = latest_messages.\"createdAt\", \
+                \"lastMessageSenderId\" = latest_messages.\"senderId\", \
+                \"lastMessageIsRead\" = latest_messages.\"isRead\", \
+                \"lastMessageType\" = latest_messages.type \
+             FROM latest_messages \
+             WHERE cp.\"conversationId\" = latest_messages.\"conversationId\""
+        );
 
-    for i in 0..parsed_messages.len() {
-        let base = i * fields_count;
-        placeholders.push(format!(
-            "(${}, ${}, ${}, ${}, ${}, ${}::\"MessageType\")",
-            base + 1, base + 2, base + 3, base + 4, base + 5, base + 6
-        ));
+        let mut query = sqlx::query(&sql);
+
+        for msg in parsed_messages {
+            query = query
+                .bind(msg.content)
+                .bind(msg.conversation_id as i64)
+                .bind(msg.sender_id as i64)
+                .bind(msg.created_at as i64)
+                .bind(msg.is_read)
+                .bind(msg.r#type.as_str());
+        }
+
+        query.execute(&self.db)
+            .await
+            .map_err(|e| e.to_string())?;
+
+        println!("🎰 Общая команда выполнена! Записано и обновлено: {} шт.", inserted_len);
+        Ok(inserted_len)
     }
-    sql.push_str(&placeholders.join(", "));
 
-    let mut query = sqlx::query(&sql);
-    let inserted_len = parsed_messages.len();
 
-    for msg in parsed_messages {
-        query = query
-            .bind(msg.content)
-            .bind(msg.conversation_id as i64)
-            .bind(msg.sender_id as i64)
-            .bind(msg.created_at as i64)
-            .bind(msg.is_read)
-            .bind(msg.r#type.as_str()); 
-    }
 
-    query.execute(&self.db)
-        .await
-        .map_err(|e| e.to_string())?;
 
-    println!("🎰 Успешно записано в Postgres пакетом через каст: {} шт.", inserted_len);
-    Ok(inserted_len)
-    }
 }
 
 pub async fn start_message_queue_worker(config: AppConfig) {
